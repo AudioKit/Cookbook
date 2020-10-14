c64efece29e6b54a6a28ca8d2d3787f57a8c70d5
Aurelius Prochazka <aurephd@gmail.com>
2020-10-14 10:34:39 -0700

    Fixed @markjeschke's bug on multiple drum pad presses

--- Cookbook/Cookbook/Recipes/Drums.swift
+++ Cookbook/Cookbook/Recipes/Drums.swift
@@ -79,7 +79,7 @@ struct PadsView: View {
     var conductor: DrumsConductor
 
     var padsAction: (_ padNumber: Int) -> Void
-    @State var isDown = -1
+    @State var downPads: [Int] = []
 
     var body: some View {
         VStack(spacing: 10) {
@@ -91,16 +91,16 @@ struct PadsView: View {
                         }) {
                             ZStack {
                                 Rectangle()
-                                    .fill(Color(self.conductor.drumSamples.map({ self.isDown == row * 4 + column ? .gray : $0.color })[getPadId(row: row, column: column)]))
+                                    .fill(Color(self.conductor.drumSamples.map({ self.downPads.contains(where: { $0 == row * 4 + column }) ? .gray : $0.color })[getPadId(row: row, column: column)]))
                                 Text(self.conductor.drumSamples.map({ $0.name })[getPadId(row: row, column: column)])
                                     .foregroundColor(Color("FontColor")).fontWeight(.bold)
                             }.gesture(DragGesture(minimumDistance: 0, coordinateSpace: .local).onChanged({_ in
-                                if self.isDown != row * 4 + column {
+                                if !(self.downPads.contains(where: { $0 == row * 4 + column })) {
                                     self.padsAction(getPadId(row: row, column: column))
-                                    self.isDown = row * 4 + column
+                                    self.downPads.append(row * 4 + column)
                                 }
                             }).onEnded({_ in
-                                self.isDown = -1
+                                self.downPads.removeAll(where: { $0 == row * 4 + column })
                             }))
                         }
                     }
