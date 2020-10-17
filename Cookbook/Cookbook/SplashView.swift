3de9259c4a4603fd35c93cbd764c3f202e191b56
Mark Jeschke <mark.jeschke@miller-media.com>
2020-10-16 17:39:28 -0700

    Added AudioKit logo and icon with some UI tweaks.

---
+++ Cookbook/Cookbook/SplashView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashView.swift
+//  Cookbook
+//
+//  Created by Mark Jeschke on 10/16/20.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
