0369fb4824f8288bb580a8a1e7674ce17ecb1547
Aurelius Prochazka <aurephd@gmail.com>
2021-01-10 11:38:18 -0800

    State of the the Shaker recipe after Office Hours 2 (but before further refinement) https://www.youtube.com/watch?v=ISJo3vzaBcw

--- Cookbook/Cookbook/Recipes/Shaker.swift
+++ Cookbook/Cookbook/Recipes/Shaker.swift
@@ -2,54 +2,155 @@ import AudioKit
 import AudioKitUI
 import SwiftUI
 
+struct ShakerMetronomeData {
+    var isPlaying = false
+    var tempo: BPM = 120
+    var timeSignatureTop: Int = 4
+    var downbeatNoteNumber = MIDINoteNumber(6)
+    var beatNoteNumber = MIDINoteNumber(10)
+    var beatNoteVelocity = 100.0
+    var currentBeat = 0
+}
+
 class ShakerConductor: ObservableObject {
 
     let engine = AudioEngine()
     let shaker = Shaker()
-    let playRate = 4.0
-    var loop: CallbackLoop!
+    var callbackInst = CallbackInstrument()
+    let reverb: Reverb
+    let mixer = Mixer()
+    var sequencer = Sequencer()
 
-    @Published var isRunning = false {
+    @Published var data = ShakerMetronomeData() {
         didSet {
-            isRunning ? loop.start() : loop.stop()
+            data.isPlaying ? sequencer.play() : sequencer.stop()
+            sequencer.tempo = data.tempo
+            updateSequences()
         }
     }
 
+    func updateSequences() {
+        var track = sequencer.tracks.first!
+
+        track.length = Double(data.timeSignatureTop)
+
+        track.clear()
+        track.sequence.add(noteNumber: data.downbeatNoteNumber, position: 0.0, duration: 0.4)
+        let vel = MIDIVelocity(Int(data.beatNoteVelocity))
+        for beat in 1 ..< data.timeSignatureTop {
+            track.sequence.add(noteNumber: data.beatNoteNumber, velocity: vel, position: Double(beat), duration: 0.1)
+        }
+
+        track = sequencer.tracks[1]
+        track.length = Double(data.timeSignatureTop)
+        track.clear()
+        for beat in 0 ..< data.timeSignatureTop {
+            track.sequence.add(noteNumber: MIDINoteNumber(beat), position: Double(beat), duration: 0.1)
+        }
+
+    }
+
     init() {
-        let delay = Delay(shaker)
-        delay.time = AUValue(1.5 / playRate)
-        delay.dryWetMix = 0.7
-        delay.feedback = 0.2
-        let reverb = Reverb(delay)
-        engine.output = reverb
+        let fader = Fader(shaker)
+        fader.gain = 20.0
+
+        //        let delay = Delay(fader)
+        //        delay.time = AUValue(1.5 / playRate)
+        //        delay.dryWetMix = 0.7
+        //        delay.feedback = 0.2
+        reverb = Reverb(fader)
+
+        let _ = sequencer.addTrack(for: shaker)
+
+
+        callbackInst = CallbackInstrument(midiCallback: { (_, beat, _) in
+            self.data.currentBeat = Int(beat)
+            print(beat)
+        })
+
+        let _ = sequencer.addTrack(for: callbackInst)
+        updateSequences()
+
+        mixer.addInput(reverb)
+        mixer.addInput(callbackInst)
+
+        engine.output = mixer
+
     }
 
     func start() {
         do {
             try engine.start()
-            loop = CallbackLoop(frequency: playRate) {
-                let type = ShakerType(rawValue: MIDIByte(AUValue.random(in: 0...22))) ?? .cabasa
-                self.shaker.trigger(type: type, amplitude: Double(AUValue.random(in: 0.7...1.0)))
-            }
         } catch let err {
             Log(err)
         }
     }
 
     func stop() {
+        sequencer.stop()
         engine.stop()
-        loop.stop()
     }
 }
 
 struct ShakerView: View {
     @ObservedObject var conductor = ShakerConductor()
 
+    func name(noteNumber: MIDINoteNumber) -> String {
+        let str = "\(ShakerType(rawValue: noteNumber)!)"
+        return str.titleCase()
+    }
+
     var body: some View {
-        Text(conductor.isRunning ? "Stop" : "Start").onTapGesture {
-            conductor.isRunning.toggle()
+        VStack {
+            Text(conductor.data.isPlaying ? "Stop" : "Start").onTapGesture {
+                conductor.data.isPlaying.toggle()
+            }
+            Text("Tempo: \(Int(conductor.data.tempo))")
+            Slider(value: $conductor.data.tempo, in: 60.0 ... 240.0, label: {
+                Text("Tempo")
+            })
+            Stepper("Downbeat \(name(noteNumber: conductor.data.downbeatNoteNumber))", onIncrement: {
+                if conductor.data.downbeatNoteNumber <= 21 {
+                    conductor.data.downbeatNoteNumber += 1
+                }
+            }, onDecrement: {
+                if conductor.data.downbeatNoteNumber >= 1 {
+                    conductor.data.downbeatNoteNumber -= 1
+                }
+            })
+            Stepper("\(name(noteNumber: conductor.data.beatNoteNumber))", onIncrement: {
+                if conductor.data.beatNoteNumber <= 21 {
+                    conductor.data.beatNoteNumber += 1
+                }
+            }, onDecrement: {
+                if conductor.data.beatNoteNumber >= 1 {
+                    conductor.data.beatNoteNumber -= 1
+                }
+            })
+            Slider(value: $conductor.data.beatNoteVelocity, in: 0.0 ... 127.0, label: {
+                Text("Velocity")
+            })
+            HStack(spacing: 10) {
+                ForEach(0 ..< conductor.data.timeSignatureTop, id: \.self) { index in
+                    ZStack {
+                        Circle().foregroundColor(conductor.data.currentBeat == index ? .red : .white)
+                        Text("\(index + 1)").foregroundColor(.black)
+                    }.onTapGesture {
+                        conductor.data.timeSignatureTop = index + 1
+                    }
+                }
+                ZStack {
+                    Circle().foregroundColor(.white)
+                    Text("+").foregroundColor(.black)
+                }
+                .onTapGesture {
+                    conductor.data.timeSignatureTop += 1
+                }
+
+            }
+
+            FFTView(conductor.reverb)
         }
-        .padding()
         .navigationBarTitle(Text("Shaker"))
         .onAppear {
             self.conductor.start()
