edd3f4a12d92983704aa9ef2898a7281475d2be6
emurray2 <evan@auraaudio.io>
2021-12-25 18:03:22 -0600

    Hounding

--- Cookbook/Cookbook/Recipes/MIDITrack.swift
+++ Cookbook/Cookbook/Recipes/MIDITrack.swift
@@ -6,7 +6,10 @@ struct MIDITrackDemo: View {
     public var body: some View {
         ScrollView {
             GeometryReader { geometry in
-                MIDITrackView(trackWidth: geometry.size.width - 20, trackHeight: 200.0, fileURL: Bundle.main.url(forResource: "Demo", withExtension: "mid")!, noteZoom: 100_000)
+                MIDITrackView(trackWidth: geometry.size.width - 20,
+                              trackHeight: 200.0,
+                              fileURL: Bundle.main.url(forResource: "Demo", withExtension: "mid")!,
+                              noteZoom: 100_000)
             }
         }
         .navigationBarTitle(Text("MIDI Track View"))
