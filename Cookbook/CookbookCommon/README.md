4785888d4a168313241a0fdab925da66250858a2
Taylor Holliday <taylor@audulus.com>
2022-02-27 14:22:27 -0800

    Add local package.

---
+++ Cookbook/CookbookCommon/README.md
@@ -0,0 +1,3 @@
+# CookbookCommon
+
+A description of this package.
---
+++ Cookbook/CookbookCommon/Sources/CookbookCommon/CookbookCommon.swift
@@ -0,0 +1,6 @@
+public struct CookbookCommon {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
