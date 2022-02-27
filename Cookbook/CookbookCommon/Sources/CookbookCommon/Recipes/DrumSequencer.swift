481113cb0831b278460e056d5e18393ca7553cfa
Taylor Holliday <taylor@audulus.com>
2022-02-27 15:31:49 -0800

    Fix URL.

--- Cookbook/CookbookCommon/Sources/CookbookCommon/Recipes/DrumSequencer.swift
+++ Cookbook/CookbookCommon/Sources/CookbookCommon/Recipes/DrumSequencer.swift
@@ -9,7 +9,7 @@ import SwiftUI
 class DrumSequencerConductor: ObservableObject {
     let engine = AudioEngine()
     let drums = MIDISampler(name: "Drums")
-    let sequencer = AppleSequencer(filename: "4tracks")
+    let sequencer = AppleSequencer(fromURL: Bundle.module.url(forResource: "Resources/4tracks", withExtension: "mid")!)
 
     @Published var tempo: Float = 120 {
         didSet {
