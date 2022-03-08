0e71562ff4998608145de51f2a4f3185d32a2833
NickCulbertson <nickculbertson@gmail.com>
2022-03-08 08:44:30 -0500

    Added square.exs

--- Cookbook/CookbookCommon/Sources/CookbookCommon/Recipes/InstrumentEXS.swift
+++ Cookbook/CookbookCommon/Sources/CookbookCommon/Recipes/InstrumentEXS.swift
@@ -24,12 +24,10 @@ class InstrumentEXSConductor: ObservableObject, KeyboardDelegate {
     func start() {
         // Load EXS file (you can also load SoundFonts and WAV files too using the AppleSampler Class)
         do {
-            let fileURL = Bundle.module.url(forResource: "Samples/Plucked Acoustic Guitar/Sampler Instruments/Plucked_Acoustic_Guitar-TField", withExtension: "exs")
-                try instrument.loadMelodicSoundFont("Samples/IbanezRG350EX", preset: 0, in: Bundle.module)
-                try instrument.loadEXS24(url: fileURL!)
-//            }
+            let fileURL = Bundle.module.url(forResource: "Sounds/Sampler Instruments/square", withExtension: "exs")
+                try instrument.loadInstrument(url: fileURL!)
         } catch {
-            Log("Could not load EXS24")
+            Log("Could not load instrument")
         }
 
         do {
@@ -63,6 +61,6 @@ struct InstrumentEXSView: View {
 
 struct InstrumentEXSView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        InstrumentEXSView()
     }
 }
