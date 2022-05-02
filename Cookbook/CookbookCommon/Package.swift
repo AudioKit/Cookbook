a8237d5105fd4eb5bba677ab4e2bbebc5c71f7e8
Aurelius Prochazka <aurephd@gmail.com>
2022-05-02 13:14:43 -0700

    Changed min target

--- Cookbook/CookbookCommon/Package.swift
+++ Cookbook/CookbookCommon/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "CookbookCommon",
-    platforms: [.macOS(.v11), .iOS(.v14), .tvOS(.v11)],
+    platforms: [.macOS(.v11), .iOS(.v15), .tvOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
