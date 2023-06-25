94881afcaac0cc415ffe956d35dfcb9ed937c642
Aurelius Prochazka <aure@aure.com>
2023-06-25 09:24:13 -0700

    Fixes for Xcode15

--- Cookbook/CookbookCommon/Package.swift
+++ Cookbook/CookbookCommon/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .package(url: "https://github.com/AudioKit/AudioKitUI",        from: "0.3.4"),
         .package(url: "https://github.com/AudioKit/AudioKitEX",        from: "5.6.0"),
         .package(url: "https://github.com/AudioKit/Controls",          from: "1.0.0"),
-        .package(url: "https://github.com/AudioKit/DunneAudioKit",     from: "5.5.0"),
+        .package(url: "https://github.com/AudioKit/DunneAudioKit",     from: "5.6.0"),
         .package(url: "https://github.com/AudioKit/Keyboard",          from: "1.3.0"),
         .package(url: "https://github.com/AudioKit/SoundpipeAudioKit", from: "5.6.0"),
         .package(url: "https://github.com/AudioKit/SporthAudioKit",    from: "5.5.0"),
