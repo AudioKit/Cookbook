8cb1fd814f17fcadaf0bb2b2227ede8e52a07982
Mark Jeschke <markjeschke@yahoo.com>
2024-06-29 23:43:36 -0700

    Added verticalSizeClass so that when the About AudioKit is in landscape orientation, the graphics appear to the left of the text.

--- Cookbook/CookbookCommon/Sources/CookbookCommon/AudioKitInfoView.swift
+++ Cookbook/CookbookCommon/Sources/CookbookCommon/AudioKitInfoView.swift
@@ -3,27 +3,22 @@ import SwiftUI
 struct AudioKitInfoView: View {
     
     @Environment(\.dismiss) var dismiss
+    @Environment(\.verticalSizeClass) var verticalSizeClass
+    @Environment(\.dynamicTypeSize) var dynamicTypeSize
+    
+    let stackSpacing: CGFloat = 25
 
     var body: some View {
         NavigationStack {
             ScrollView {
-                VStack(spacing: 25) {
-                    VStack(spacing: 20) {
-                        Image("audiokit-icon")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(maxWidth: 150)
-                        Image("audiokit-logo")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(maxWidth: 150)
+                if verticalSizeClass == .regular {
+                    VStack(spacing: stackSpacing) {
+                        AboutAudioKitContentView(stackSpacing: stackSpacing)
                     }
-                    Divider()
-                    Text("AudioKit is an audio synthesis, processing, and analysis platform for iOS, macOS, and tvOS.\n\nMost of the examples that were inside of AudioKit are now in this application.\n\nIn addition to the resources found here, there are various open-source example projects on GitHub and YouTube created by AudioKit contributors.")
-                    Spacer()
+                } else {
+                    HStack(spacing: stackSpacing) { AboutAudioKitContentView(stackSpacing: stackSpacing)
+                    }
                 }
-                .padding()
-
             }
             .toolbar {
                 ToolbarItem(placement: .primaryAction) {
@@ -40,6 +35,39 @@ struct AudioKitInfoView: View {
     }
 }
 
-#Preview {
+#Preview("AudioKit Info") {
     AudioKitInfoView()
+}
+
+struct AboutAudioKitContentView: View {
+
+    private let maxWidth: CGFloat = 200
+    var stackSpacing: CGFloat
+
+    var body: some View {
+        audioKitArtwork
+        audioKitText
+    }
+
+    private var audioKitArtwork: some View {
+        return VStack(spacing: stackSpacing) {
+            Image("audiokit-icon")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(maxWidth: maxWidth)
+            Image("audiokit-logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(maxWidth: maxWidth)
+        }
+    }
+
+    private var audioKitText: some View {
+        Text("AudioKit is an audio synthesis, processing, and analysis platform for iOS, macOS, and tvOS.\n\nMost of the examples that were inside of AudioKit are now in this application.\n\nIn addition to the resources found here, there are various open-source example projects on GitHub and YouTube created by AudioKit contributors.")
+            .padding()
+    }
+}
+
+#Preview("About AudioKit Content") {
+    AboutAudioKitContentView(stackSpacing: 25)
 }
